8f68aec5ad22fc94f7c14b71369c88b5fe07ae1c
LordOfReinkeness <post@lukas-reinke.de>
2024-12-14 10:57:07 +0100

    2024 d14

---
+++ challenges/_2024/_13/input_parse.py
@@ -0,0 +1,4 @@
+def parse_input(data: string):
+	out = data
+	# Your challenges for AOC 2024 day 13 input parsing goes here
+	return out
---
+++ challenges/_2024/_13/part_1.py
@@ -0,0 +1,7 @@
+from challenges._2024._13.input_parse import parse_input
+
+def main(data):
+	data = parse_input(data)
+	# Your challenges for AOC 2024 day 13 part 1 goes here
+
+	return 'AOC 2024 day 13 part 1'
